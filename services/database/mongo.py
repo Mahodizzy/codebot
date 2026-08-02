96566a347f4521115ea73564e956e609e2bd8f9c
Mahodizzy <133908664+Mahodizzy@users.noreply.github.com>
2026-08-02 12:44:38 -0500

    Update mongo.py

--- services/database/mongo.py
+++ services/database/mongo.py
@@ -1,8 +1,25 @@
 from pymongo import MongoClient
-from config import MONGO_URI, DATABASE_NAME
 
-client = MongoClient(MONGO_URI)
+from config import (
+    MONGO_URI,
+    DATABASE_NAME
+)
 
-db = client[DATABASE_NAME]
+
+# Conexión MongoDB
+
+cliente = MongoClient(
+    MONGO_URI
+)
+
+
+# Base de datos
+
+db = cliente[
+    DATABASE_NAME
+]
+
+
+# Colección usuarios
 
 usuarios = db["usuarios"]
