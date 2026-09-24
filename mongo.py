f6ef16ed6cc54dd069a139a3082f87a8cba815ba
Mahodizzy <133908664+Mahodizzy@users.noreply.github.com>
2026-09-23 23:28:14 -0500

    Delete mongo.py

--- mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-from config import MONGO_URI, DATABASE_NAME
-
-cliente = MongoClient(MONGO_URI, serverSelectionTimeoutMS=10000)
-db = cliente[DATABASE_NAME]
-
-# Colecciones
-usuarios = db.usuarios
